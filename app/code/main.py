c7d111f3af6681cf0f5dbee7fa071330cddde2e1
kei selami <kei.selami@eurostep.it>
2024-01-12 17:54:59 +0100

    Fix the creation of just desired subplots and better naming of images created

--- app/code/main.py
+++ app/code/main.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 import matplotlib.pyplot as plt
 import pandas as pd
 
@@ -53,85 +54,97 @@ def get_data_for_country(country):
     return cases_by_location, deaths_by_location, recovered_by_location
 
 
-def make_confirmed_cases_plots(axs, cases_by_location):
+def make_confirmed_cases_plots(daily_confirmed_cases_plot, total_confirmed_cases_plot, cases_by_location):
+    print('Generating subplots for confirmed cases.')
+
     # Plot 1: Daily confirmed cases.
     cases_by_location.index = pd.to_datetime(cases_by_location.index, format=DATE_FORMAT, errors='coerce')
     new_daily_confirmed_cases = cases_by_location.diff().fillna(0)
 
-    axs[0, 0].bar(cases_by_location.index, new_daily_confirmed_cases.values, label='Daily Confirmed Cases',color='purple')
-    axs[0, 0].set_ylabel('Daily Confirmed Cases')
-    axs[0, 0].legend()
-    axs[0, 0].ticklabel_format(style='plain', axis='y')  # Disable scientific notation.
+    daily_confirmed_cases_plot.bar(cases_by_location.index, new_daily_confirmed_cases.values, label='Daily Confirmed Cases',color='purple')
+    daily_confirmed_cases_plot.set_ylabel('Daily Confirmed Cases')
+    daily_confirmed_cases_plot.legend()
+    daily_confirmed_cases_plot.ticklabel_format(style='plain', axis='y')  # Disable scientific notation.
 
     # Plot 2: Total confirmed cases.
-    axs[0, 1].plot(cases_by_location.index, cases_by_location.values, label='Total Confirmed Cases', color='blue')
-    axs[0, 1].set_ylabel('Confirmed cases, $N$')
-    axs[0, 1].legend()
-    axs[0, 1].ticklabel_format(style='plain', axis='y')  # Disable scientific notation.
+    total_confirmed_cases_plot.plot(cases_by_location.index, cases_by_location.values, label='Total Confirmed Cases', color='blue')
+    total_confirmed_cases_plot.set_ylabel('Confirmed cases, $N$')
+    total_confirmed_cases_plot.legend()
+    total_confirmed_cases_plot.ticklabel_format(style='plain', axis='y')  # Disable scientific notation.
 
     # Set major locator for the x-axis to be every 6 months.
-    axs[0, 0].xaxis.set_major_locator(plt.MultipleLocator(180))
-    axs[0, 1].xaxis.set_major_locator(plt.MultipleLocator(180))
+    daily_confirmed_cases_plot.xaxis.set_major_locator(plt.MultipleLocator(180))
+    total_confirmed_cases_plot.xaxis.set_major_locator(plt.MultipleLocator(180))
 
 
-def make_deaths_plots(axs, deaths_by_location):
+def make_deaths_plots(daily_deaths_plot, total_deaths_plot, deaths_by_location):
+    print('Generating subplots for deaths cases.')
     # Plot 3: Daily deaths.
     deaths_by_location.index = pd.to_datetime(deaths_by_location.index, format=DATE_FORMAT, errors='coerce')
     new_daily_deaths = deaths_by_location.diff().fillna(0)
 
-    axs[1, 0].bar(deaths_by_location.index, new_daily_deaths.values, label='Daily Deaths', color='purple')
-    axs[1, 0].set_ylabel('Daily Deaths')
-    axs[1, 0].legend()
-    axs[1, 0].ticklabel_format(style='plain', axis='y')  # Disable scientific notation.
+    daily_deaths_plot.bar(deaths_by_location.index, new_daily_deaths.values, label='Daily Deaths', color='purple')
+    daily_deaths_plot.set_ylabel('Daily Deaths')
+    daily_deaths_plot.legend()
+    daily_deaths_plot.ticklabel_format(style='plain', axis='y')  # Disable scientific notation.
 
     # Plot 4: Total deaths.
-    axs[1, 1].plot(deaths_by_location.index, deaths_by_location.values, label='Total Deaths', color='orange')
-    axs[1, 1].set_xlabel('Date')
-    axs[1, 1].set_ylabel('Total Deaths')
-    axs[1, 1].legend()
-    axs[1, 1].ticklabel_format(style='plain', axis='y')  # Disable scientific notation.
+    total_deaths_plot.plot(deaths_by_location.index, deaths_by_location.values, label='Total Deaths', color='orange')
+    total_deaths_plot.set_xlabel('Date')
+    total_deaths_plot.set_ylabel('Total Deaths')
+    total_deaths_plot.legend()
+    total_deaths_plot.ticklabel_format(style='plain', axis='y')  # Disable scientific notation.
 
     # Set major locator for the x-axis to be every 6 months.
-    axs[1, 0].xaxis.set_major_locator(plt.MultipleLocator(180))
-    axs[1, 1].xaxis.set_major_locator(plt.MultipleLocator(180))
+    daily_deaths_plot.xaxis.set_major_locator(plt.MultipleLocator(180))
+    total_deaths_plot.xaxis.set_major_locator(plt.MultipleLocator(180))
 
 
-def make_recovered_plots(axs, recovered_by_location):
+def make_recovered_plots(daily_recovered_plot, total_recovered_plot, recovered_by_location):
+    print('Generating subplots for recovered cases.')
+
     # Plot 5: Daily recovered.
     recovered_by_location.index = pd.to_datetime(recovered_by_location.index, format=DATE_FORMAT, errors='coerce')
     new_daily_recoveries = recovered_by_location.diff().fillna(0)
 
-    axs[2, 0].bar(recovered_by_location.index, new_daily_recoveries.values, label='Daily Recoveries', color='green')
-    axs[2, 0].set_xlabel('Date')
-    axs[2, 0].set_ylabel('Daily Recoveries')
-    axs[2, 0].legend()
-    axs[2, 0].ticklabel_format(style='plain', axis='y')  # Disable scientific notation.
+    daily_recovered_plot.bar(recovered_by_location.index, new_daily_recoveries.values, label='Daily Recoveries', color='green')
+    daily_recovered_plot.set_xlabel('Date')
+    daily_recovered_plot.set_ylabel('Daily Recoveries')
+    daily_recovered_plot.legend()
+    daily_recovered_plot.ticklabel_format(style='plain', axis='y')  # Disable scientific notation.
 
     # Plot 6: Total recoveries.
-    axs[2, 1].plot(recovered_by_location.index, recovered_by_location.values, label='Total Recoveries', color='cyan')
-    axs[2, 1].set_xlabel('Date')
-    axs[2, 1].set_ylabel('Total Recoveries')
-    axs[2, 1].legend()
-    axs[2, 1].ticklabel_format(style='plain', axis='y')  # Disable scientific notation.
+    total_recovered_plot.plot(recovered_by_location.index, recovered_by_location.values, label='Total Recoveries', color='cyan')
+    total_recovered_plot.set_xlabel('Date')
+    total_recovered_plot.set_ylabel('Total Recoveries')
+    total_recovered_plot.legend()
+    total_recovered_plot.ticklabel_format(style='plain', axis='y')  # Disable scientific notation.
 
     # Set major locator for the x-axis to be every 6 months.
-    axs[2, 0].xaxis.set_major_locator(plt.MultipleLocator(180))
-    axs[2, 1].xaxis.set_major_locator(plt.MultipleLocator(180))
+    daily_recovered_plot.xaxis.set_major_locator(plt.MultipleLocator(180))
+    total_recovered_plot.xaxis.set_major_locator(plt.MultipleLocator(180))
 
 
-def make_plots(country, confirmed_cases, deaths, recoveries):
-    fig, axs = plt.subplots(3, 2, figsize=(15, 12), sharex=True)
+def generate_plot(country, confirmed_cases, deaths, recoveries):
+    print(f'Generating plot for {country}')
+
+    count_of_plots_needed = sum((confirmed_cases, deaths, recoveries))
+    current_plots_row = 0
+
+    fig, axs = plt.subplots(count_of_plots_needed, 2, figsize=(15, 12), sharex=True)
 
     cases_by_location, deaths_by_location, recovered_by_location = get_data_for_country(country)
 
     if confirmed_cases:
-        make_confirmed_cases_plots(axs, cases_by_location)
+        make_confirmed_cases_plots(axs[current_plots_row, 0], axs[current_plots_row, 1], cases_by_location)
+        current_plots_row += 1
 
     if deaths:
-        make_deaths_plots(axs, deaths_by_location)
+        make_deaths_plots(axs[current_plots_row, 0], axs[current_plots_row, 1], deaths_by_location)
+        current_plots_row += 1
 
     if recoveries:
-        make_recovered_plots(axs, recovered_by_location)
+        make_recovered_plots(axs[current_plots_row, 0], axs[current_plots_row, 1], recovered_by_location)
 
     # Change the format of the x-axis labels to display month and year.
     for ax in axs.flat:
@@ -142,8 +155,26 @@ def make_plots(country, confirmed_cases, deaths, recoveries):
 
     plt.suptitle('title')
 
+
+def store_plot_to_storage(country, confirmed_cases, deaths, recoveries):
+    # Create the images directory if it doesn't exist
+    images_dir = '/app/images'
+    os.makedirs(images_dir, exist_ok=True)
+
+    # Construct the image name based on the flags
+    image_name = f"{country}_"
+    if confirmed_cases:
+        image_name += "c"
+    if deaths:
+        image_name += "d"
+    if recoveries:
+        image_name += "r"
+
+    # Add the plot suffix
+    image_name += "_plot.png"
+
     # Save the plot as an image file.
-    image_path = '/app/plot.png'
+    image_path = os.path.join(images_dir, image_name)
     plt.savefig(image_path)
 
     # Print the path to the saved image.
@@ -159,5 +190,6 @@ if __name__ == "__main__":
 
     countries = args.countries.split(',')
     for country in countries:
-        print(f'Generating plots for {country}')
-        make_plots(country, args.c, args.d, args.r)
+        generate_plot(country, args.c, args.d, args.r)
+
+        store_plot_to_storage(country, args.c, args.d, args.r)
